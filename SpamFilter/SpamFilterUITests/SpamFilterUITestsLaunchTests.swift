2eb70e34b888b220ecb5823536875afef5c46124
Jaewon <j.won@navercorp.com>
2022-02-16 10:23:18 +0900

    chore: initial project

---
+++ SpamFilter/SpamFilterUITests/SpamFilterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpamFilterUITestsLaunchTests.swift
+//  SpamFilterUITests
+//
+//  Created by J.won on 2022/02/16.
+//
+
+import XCTest
+
+class SpamFilterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
