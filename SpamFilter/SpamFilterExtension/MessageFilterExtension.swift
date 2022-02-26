78fc194a962ad2868da2eb1efeec0984c17961e0
Jaewon <j.won@navercorp.com>
2022-02-26 15:57:48 +0900

    feature: 내부적으로 등록한 스팸 키워드 거를 수 있도록 구현

---
+++ SpamFilter/SpamFilterExtension/MessageFilterExtension.swift
@@ -0,0 +1,67 @@
+//
+//  MessageFilterExtension.swift
+//  SpamFilterExtension
+//
+//  Created by J.won on 2022/02/26.
+//
+
+import IdentityLookup
+
+final class MessageFilterExtension: ILMessageFilterExtension {
+    private let spamWordsList = ["무료 거부", "무료거부", "광고"]
+}
+
+extension MessageFilterExtension: ILMessageFilterQueryHandling {
+
+    func handle(_ queryRequest: ILMessageFilterQueryRequest, context: ILMessageFilterExtensionContext, completion: @escaping (ILMessageFilterQueryResponse) -> Void) {
+        // First, check whether to filter using offline data (if possible).
+        let offlineAction = self.offlineAction(for: queryRequest)
+
+        switch offlineAction {
+        case .allow, .junk, .promotion, .transaction:
+            // Based on offline data, we know this message should either be Allowed, Filtered as Junk, Promotional or Transactional. Send response immediately.
+            let response = ILMessageFilterQueryResponse()
+            response.action = offlineAction
+
+            completion(response)
+
+        case .none:
+            // Based on offline data, we do not know whether this message should be Allowed or Filtered. Defer to network.
+            // Note: Deferring requests to network requires the extension target's Info.plist to contain a key with a URL to use. See documentation for details.
+            context.deferQueryRequestToNetwork() { (networkResponse, error) in
+                let response = ILMessageFilterQueryResponse()
+                response.action = .none
+
+                if let networkResponse = networkResponse {
+                    // If we received a network response, parse it to determine an action to return in our response.
+                    response.action = self.action(for: networkResponse)
+                } else {
+                    NSLog("Error deferring query request to network: \(String(describing: error))")
+                }
+
+                completion(response)
+            }
+
+        @unknown default:
+            break
+        }
+    }
+
+    private func offlineAction(for queryRequest: ILMessageFilterQueryRequest) -> ILMessageFilterAction {
+        guard let messageBody = queryRequest.messageBody else { return .allow }
+        
+        for spamWord in spamWordsList {
+            if messageBody.contains(spamWord) {
+                return .junk
+            }
+        }
+        
+        return .allow
+    }
+
+    private func action(for networkResponse: ILNetworkResponse) -> ILMessageFilterAction {
+        // Replace with logic to parse the HTTP response and data payload of `networkResponse` to return an action.
+        return .none
+    }
+
+}
